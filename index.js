49f78bf0183259722252971924b467cd1d774b06
Saurav Kumar <saurav25400@gmail.com>
2024-05-25 18:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server=express();
 
 const PORT=process.env.PORT||5000
 const corsOptions = {
-    origin: 'http://localhost:3000', // frontend URL
+    origin: 'https://main--assignmentfrontend123.netlify.app/', // frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
